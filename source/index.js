79930e7db37f91522063b99f1558e37367dc2e27
Onyeka Obi <oobi@BELC02Q6GHWG8WN.sea.corp.expecn.com>
2018-10-13 19:18:19 -0700

    Run shell script from tmp

--- source/index.js
+++ source/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.handler = function (event, context, callback) {
-    require('child_process').execFile('ls', function(err, data) {
+    require('child_process').exec('cp hi.sh /tmp; cd /tmp; chmod +x hi.sh; /tmp/hi.sh', function(err, data) {
       function responseBody() {
         if (err) return err;
         else return data.toString();
