2b263e3cad210a82c3441c8623c3c3d15d4bc1f0
Onyeka Obi <oobi@BELC02Q6GHWG8WN.sea.corp.expecn.com>
2019-03-24 17:33:49 -0700

    Source from S3

--- lambda.tf
+++ lambda.tf
@@ -1,24 +1,42 @@
 locals {
-  lambda-file = "lambda-con.zip"
+  artifact-acl = "public-read"
+  artifact-bucket = "oni-lambda-deploy"
+  artifact-key = "lambda-con.zip"
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-west-1"
 }
 
 data "archive_file" "lambda-con" {
   type = "zip"
   source_dir = "./source"
-  output_path = "${local.lambda-file}"
+  output_path = "${local.artifact-key}"
+}
+
+resource "aws_s3_bucket" "mojave" {
+  bucket = "${local.artifact-bucket}"
+  acl    = "${local.artifact-acl}"
+}
+
+resource "aws_s3_bucket_object" "object" {
+  acl = "${local.artifact-acl}"
+  bucket = "${aws_s3_bucket.mojave.bucket}"
+  key = "${local.artifact-key}"
+  source = "./${local.artifact-key}"
+  etag = "${md5(file("./${local.artifact-key}"))}"
+  content_type = "application/octet-stream"
 }
 
 resource "aws_lambda_function" "lambda-test" {
-  filename = "${local.lambda-file}"
+  s3_bucket = "${aws_s3_bucket.mojave.bucket}"
+  s3_key = "${local.artifact-key}"
   function_name = "lambda-test"
   role = "${aws_iam_role.lambda-role.arn}"
   handler = "index.handler"
   source_code_hash = "${data.archive_file.lambda-con.output_base64sha256}"
   runtime = "nodejs6.10"
+  depends_on = ["aws_s3_bucket_object.object"]
 }
 
 resource "aws_iam_role" "lambda-role" {
